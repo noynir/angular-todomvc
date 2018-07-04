5dedc2cdf7c78be17b5894fd2d2e921bdef7083b
Nir Noy <nirn@sela.co.il>
2018-07-04 23:36:40 +0300

    starter step2

--- projects/step2/src/app/todo/todo.module.ts
+++ projects/step2/src/app/todo/todo.module.ts
@@ -6,12 +6,10 @@ import { EffectsModule } from '@ngrx/effects';
 import { TodoContainerComponent } from './todo-container/todo-container.component';
 import { TasksComponent } from './tasks/tasks.component';
 import * as fromTodo from './reducers';
-import { TaskEffects } from './effects/task.effects';
 
 @NgModule({
 	imports: [ CommonModule,
 		FormsModule,
-		EffectsModule.forFeature([TaskEffects]),
 		StoreModule.forFeature('todo', fromTodo.reducers, { metaReducers: fromTodo.metaReducers })
 	],
 	declarations: [TodoContainerComponent, TasksComponent],
